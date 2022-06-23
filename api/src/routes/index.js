58f45d0016ae4443ce28904179872ac1c29eb414
dunghenry <trandungksnb6930082001@gmail.com>
2022-06-23 15:38:13 +0700

    done and test auth

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,8 +1,10 @@
 const auth = require('./auth');
+const user = require('./user');
 const express = require('express');
 const router = express.Router();
 const routes = (app) => {
     app.use('/api/auth', auth);
+    app.use('/api/users', user);
     
 }
 module.exports = routes
